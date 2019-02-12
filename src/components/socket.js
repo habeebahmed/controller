4185cd2ce413d28cc5c424d48a647a6bd5941a32
habeeb ahmed <habeebahmed543@gmail.com>
2019-02-12 10:35:40 +0530

    Build added

--- src/components/socket.js
+++ src/components/socket.js
@@ -4,7 +4,7 @@ var socket = null;
 
 
 const socketStart = (code) => {
-    socket = io('http://192.168.43.243:7777')
+    socket = io('http://192.168.43.80:7777')
     socket.on('connect', (res) => {
         console.log('Connection Established')    
         socket.emit('CONTROLLER_START_UP', { sessionId: code });
